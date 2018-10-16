29e715bd3e99dbb3bcf8edcf68c1cd1e924e0af3
kindkmj <kindcode@gmail.com>
2018-10-16 10:07:22 +0900

    v 1.4.6 Visible 충돌 오류 은지가 수정해주었음.  역시 갓 은지님 Mainform은 항상 종료를 시켜줘야 obj와 bin의 충돌오류가 나오지 않게됨.

--- PowerString/MainTitle.cs
+++ PowerString/MainTitle.cs
@@ -19,14 +19,24 @@ namespace PowerString
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void PracticeProblem_Click(object sender, EventArgs e)
         {
-            SelectionOfQuiz selectionOfQuiz = new SelectionOfQuiz(); //버튼을 클릭하면 3번화면이 나오게됨.
-            selectionOfQuiz.Show();//model
-            this.Hide();
+            SelectionOfQuiz selectionOfQuiz = new SelectionOfQuiz(this); //버튼을 클릭하면 3번화면이 나오게됨.
+            this.Visible = false;
+            selectionOfQuiz.Visible = true;
+            selectionOfQuiz.Show();
+
+            MainTitle mainTitle = this;
+            //mainTitle.Close();
+            
+
+            //selectionOfQuiz.Location = new Point(100, 100);
+            /*Controls.Add(selectionOfQuiz);//유저컨트롤 추가하는부분
+            Controls.Remove(selectionOfQuiz);//유저컨트롤 제거하는 부분.*/
+
         }
 
         private void goToUserInfo_Click(object sender, EventArgs e)
@@ -35,6 +45,12 @@ namespace PowerString
             // 띄운 유저 창을 닫을 때 까지 부모창에는 접근할 수 없음. 
             UserInfo userInfo = new UserInfo();
             userInfo.Show();
+            
+            /*var form = new UserInfo();
+            var main = this.Location;
+
+            form.Location = new Point((main.X + 600), main.Y);
+            form.Show();*/
         }
     }
 }
--- PowerString/SelectionOfQuiz.cs
+++ PowerString/SelectionOfQuiz.cs
@@ -12,22 +12,32 @@ namespace PowerString
 {
     public partial class SelectionOfQuiz : Form
     {
-        public SelectionOfQuiz()
+        private MainTitle _mt;
+
+        public SelectionOfQuiz(MainTitle mainForm)
         {
+            _mt = mainForm;
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
+            _mt.Close();
         }
 
         private void MainTitleReturnButton_Click(object sender, EventArgs e)
         {
-            MainTitle mt = new MainTitle(); //뒤로가기 버튼을 누르게 되면 최초의 화면이 나오게 됨.
-            mt.Show();
-            this.Hide();
+            //MainTitle mt = new MainTitle(); //뒤로가기 버튼을 누르게 되면 최초의 화면이 나오게 됨.
+            /*this.Visible = false;
+            mt.Visible = true;*/
+            _mt.Visible = true;
+            _mt.Location = new Point(100, 100);
+            this.Visible = false;
         }
+
+       
+
 
         private void MultiGameButton_Click(object sender, EventArgs e)
         {
