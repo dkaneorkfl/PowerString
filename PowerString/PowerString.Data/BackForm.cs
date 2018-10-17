79a66ee38bf64abcd1440c002fef2dbad27ab84d
SK O <mnbvcxzjjkk312@hotmail.com>
2018-10-17 15:35:39 +0900

    Ver. 1.4.15 작성자 오성규 1. BackForm 생성 및 Invisible 코드 작성.

---
+++ PowerString/PowerString.Data/BackForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerString.Data
+{
+    public partial class BackForm : Form
+    {
+        public BackForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BackForm_Load(object sender, EventArgs e)
+        {
+            BackForm bf = new BackForm();
+            bf.Show();
+        }
+
+        private void BackForm_Shown(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+    }
+}
