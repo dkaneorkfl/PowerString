b1402ce988e6d5ce62fe9513dc2f0c6689e18501
kindkmj <kindcode@gmail.com>
2018-10-18 10:08:18 +0900

    v1.0.2? 회원가입시 점수도 0점으로 같이 저장하게끔 변경하였습니다.
    각 코드마다 주석을 사용해서 설명을 첨부하엿습니다.
    
    -실행시 데이터가 저장됩니다.

--- PowerString/SignUpForm.cs
+++ PowerString/SignUpForm.cs
@@ -16,31 +16,35 @@ namespace PowerString
     {
         private Tester newUser = new Tester();
         private bool CloseCheck = false;
+        private StartForm _mt;
 
         void CreateUser()
         {
-            string IdInputText = IdInputBox.Text;
-            string PswInputText = PswInputBox.Text;
-            string PswInputAgainText = PswInputAgainBox.Text;
-
-            if (PswInputText != PswInputAgainText)
+            string IdInputText = IdInputBox.Text; //아이디,name을 저장하는 부분입니다.
+            string PswInputText = PswInputBox.Text; //비밀번호를 저장하는 부분입니다.
+            string PswInputAgainText = PswInputAgainBox.Text; //비밀번호가 앞의 번호와 맞는지 확인하는 부분입니다.
+            if (PswInputText != PswInputAgainText) //입력햇던 번호와 다시 입력햇던 번호가 같은지 다른지 확인합니다. 
             {
+                //비밀번호가 다르다면 아래의 멘트가 나오게 됩니다.
                 MessageBox.Show("비밀번호가 틀렷습니다. 다시 입력해주세요");
             }
             else
             {
-                MessageBox.Show("가입을 축하합니다.");
-                this.Visible = false;
-                _mt.Visible = true;
-                _mt.Location = new Point(100, 100);
-                this.Visible = false;
-                CloseCheck = true;
-                newUser.TesterName = IdInputText;
-                newUser.TesterPassword = PswInputText;
-                DataRepository.Tester.Insert(newUser);
+                //비밀번호가 같으면 아래의 부분이 실행됩니다.
+
+                MessageBox.Show("가입을 축하합니다."); //가입을 축하하는 멘트를 보냅니다.
+                _mt.Visible = true;  //시작화면을 보여주게 됩니다.
+                _mt.Location = new Point(100, 100); // 시작화면이 보여주는 위치를 지정해 줍니다
+                this.Visible = false; //현재의 회원가입화면을 숨기게 됩니다.
+                CloseCheck = true; //회원가입 폼을 강제종료하게될경우 메인폼이 종료되게끔 실행하는 폼 입니다.
+
+                int ScoreClear = 0; // 점수는 0점으로 초기화 시켜줍니다.
+                newUser.TesterName = IdInputText; // name 을 데이터베이스에 저장하는 코드입니다.
+                newUser.TesterPassword = PswInputText; //비밀번호를 데이터베이스에 저장하는 코드입니다.
+                newUser.TesterScore = ScoreClear; //최초의 점수 0 점을 저장하는 코드입니다.
+                DataRepository.Tester.Insert(newUser); // 이름과 비밀번호 점수를 저장하도록 실행시켜주는 코드입니다.
             }
         }
-        private StartForm _mt;
 
         public SignUpForm(StartForm mainForm)
         {
@@ -51,12 +55,7 @@ namespace PowerString
 
         private void NewAccountCreateBtn_Click(object sender, EventArgs e)
         {
-            CreateUser();
-            //Insert();
-
-            string IdInputText = IdInputBox.Text;
-            //context.Testers.Add(IdInputText);
-
+            CreateUser(); // 버튼을 누르게 되면 해당 메소드를 실행하게 됩니다.
         }
 
 
@@ -65,7 +64,8 @@ namespace PowerString
 
         }
 
-        #region HintMessage
+        //textbox의 hint를 담당하는 항목입니다.
+        #region HintMessage 
         private void IdInputBox_Enter_1(object sender, EventArgs e)
         {
             if (IdInputBox.Text == "사용자명은 30자까지")
@@ -141,6 +141,6 @@ namespace PowerString
             CloseCheck = false;
             Application.Exit();
         }
-        #endregion
+        #endregion 
     }
 }
