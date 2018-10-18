f490a03d990ddcf723c9e75b3b561832af6b9369
kindkmj <kindcode@gmail.com>
2018-10-18 10:38:20 +0900

    v1.0.3? private 에 변수이름명에 _ 를 붙였습니다. 비밀번호가 4자가 넘어가게 된다면 메시지박스가 출력됩니다. 비밀번호가 4자 이하이며 비밀번호와 확인부분이 같다면 회원가입이 정상적으로 실행됩니다.

--- PowerString/SignUpForm.cs
+++ PowerString/SignUpForm.cs
@@ -14,8 +14,8 @@ namespace PowerString
 {
     public partial class SignUpForm : Form
     {
-        private Tester newUser = new Tester();
-        private bool CloseCheck = false;
+        private Tester _newUser = new Tester();
+        private bool _CloseCheck = false;
         private StartForm _mt;
 
         void CreateUser()
@@ -30,19 +30,26 @@ namespace PowerString
             }
             else
             {
-                //비밀번호가 같으면 아래의 부분이 실행됩니다.
+                if (4 < PswInputText.Length)//비밀번호가 4자보다 크다면 아래의 메시지박스가 실행됩니다.
+                {
+                    MessageBox.Show("글자수는 4개 이하로 해주세요");
+                }
+                //글자수가 4자이하이고 비밀번호와 비밀번호확인이 같다면 아래의 코드가 실행됩니다.
+                else if ((PswInputText == PswInputAgainText) && (PswInputText.Length <= 4))
+                {
+                    MessageBox.Show("가입을 축하합니다."); //가입을 축하하는 멘트를 보냅니다.
+                    _mt.Visible = true;  //시작화면을 보여주게 됩니다.
+                    _mt.Location = new Point(100, 100); // 시작화면이 보여주는 위치를 지정해 줍니다
+                    this.Visible = false; //현재의 회원가입화면을 숨기게 됩니다.
+                    _CloseCheck = true; //회원가입 폼을 강제종료하게될경우 메인폼이 종료되게끔 실행하는 폼 입니다.
 
-                MessageBox.Show("가입을 축하합니다."); //가입을 축하하는 멘트를 보냅니다.
-                _mt.Visible = true;  //시작화면을 보여주게 됩니다.
-                _mt.Location = new Point(100, 100); // 시작화면이 보여주는 위치를 지정해 줍니다
-                this.Visible = false; //현재의 회원가입화면을 숨기게 됩니다.
-                CloseCheck = true; //회원가입 폼을 강제종료하게될경우 메인폼이 종료되게끔 실행하는 폼 입니다.
+                    int ScoreClear = 0; // 점수는 0점으로 초기화 시켜줍니다.
+                    _newUser.TesterName = IdInputText; // name 을 데이터베이스에 저장하는 코드입니다.
+                    _newUser.TesterPassword = PswInputText; //비밀번호를 데이터베이스에 저장하는 코드입니다.
+                    _newUser.TesterScore = ScoreClear; //최초의 점수 0 점을 저장하는 코드입니다.
+                    DataRepository.Tester.Insert(_newUser); // 이름과 비밀번호 점수를 저장하도록 실행시켜주는 코드입니다.
+                }
 
-                int ScoreClear = 0; // 점수는 0점으로 초기화 시켜줍니다.
-                newUser.TesterName = IdInputText; // name 을 데이터베이스에 저장하는 코드입니다.
-                newUser.TesterPassword = PswInputText; //비밀번호를 데이터베이스에 저장하는 코드입니다.
-                newUser.TesterScore = ScoreClear; //최초의 점수 0 점을 저장하는 코드입니다.
-                DataRepository.Tester.Insert(newUser); // 이름과 비밀번호 점수를 저장하도록 실행시켜주는 코드입니다.
             }
         }
 
@@ -138,7 +145,7 @@ namespace PowerString
 
         private void SignUpForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            CloseCheck = false;
+            _CloseCheck = false;
             Application.Exit();
         }
         #endregion 
