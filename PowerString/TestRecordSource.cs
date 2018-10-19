8c180a072fb1114f07db7338d67c84890c41f301
EunJi Yoo <doeltms1@gmail.com>
2018-10-19 19:32:09 +0900

    2018.10.19 오후 7:30 유은지 1. UserInfo 폼 Filter 기능 구현 2. UserInfo 폼 코드 정리

---
+++ PowerString/TestRecordSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerString
+{
+    public class TestRecordSource
+    {
+        public int CodeId { get; set; }
+        public String TestRecordDate { get; set; }
+        public bool TestRecordIsCorrect { get; set; }
+        public Nullable<decimal> TimerTaken { get; set; }
+
+        public TestRecordSource(int codeId, string testRecordDate, bool testRecordIsCorrect, decimal? timerTaken)
+        {
+            CodeId = codeId;
+            TestRecordDate = testRecordDate;
+            TestRecordIsCorrect = testRecordIsCorrect;
+            TimerTaken = timerTaken;
+        }
+    }
+}
